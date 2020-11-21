dddcca33630d0c128415a6e525cb62614fb0c599
DMess174 <dmess174@gmail.com>
2020-11-21 11:58:48 +0500

    Create ViewModelBase

---
+++ DMess.Supervision.Desktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DMess.Supervision.Desktop.ViewModels
+{
+    public delegate TViewModel CreateViewModel<TViewModel>() where TViewModel : ViewModelBase;
+
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
